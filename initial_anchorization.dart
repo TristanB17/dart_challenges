6960ca8ae0de57df7186945e7f6c44f66d6adbaf
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2019-01-11 21:27:50 -0700

    Made some modifications to existing functions

--- initial_anchorization.dart
+++ initial_anchorization.dart
@@ -1,10 +1,16 @@
 void main(){
-  return anchorize('evil');
+  var exclamation = anchorize('eeeeeee');
+  var scouterReading = powerLevel();
+  print('${exclamation}!! The scouter reads a power level of ${scouterReading}');
 }
 
 anchorize(text){
   var replacedText = text.replaceAll(new RegExp(r'e'), 'E');
-  print(replacedText);
+  return replacedText;
+}
+
+String powerLevel(){
+  return 'Over 9000';
 }
 
 // urlForHttp(text){
