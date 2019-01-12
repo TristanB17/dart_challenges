cf0a9124ce33647359866dc8f7d4af4abc896e0e
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2019-01-11 21:55:54 -0700

    Adds some loop exploring

--- initial_anchorization.dart
+++ initial_anchorization.dart
@@ -12,25 +12,3 @@ anchorize(text){
 String powerLevel(){
   return 'Over 9000';
 }
-
-// urlForHttp(text){
-//   text.replaceAll(new RegExp(r'e'), 'E');
-// }
-
-// Parse the text and replace Globally url with corresponding html syntax.
-
-// Supported protocols:
-
-// http
-// https
-// ftp
-// ftps
-// file
-// smb
-// NOTE: Ideal will be with Regex
-
-// Input
-// 'hello http://world.com !'
-
-// Output
-// 'hello <a href="http://world.com">http://world.com</a> !'
